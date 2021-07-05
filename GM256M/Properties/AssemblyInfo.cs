bae672493cc7c0c9d96b155f403e620d2197cea8
GamingMidi <eliahwarner54@gmail.com>
2021-07-05 02:59:08 -0700

    Fixed assembly information

--- GM256M/Properties/AssemblyInfo.cs
+++ GM256M/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GM256M")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Simple MIDI Merger")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GM256M")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright  GamingMIDI©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.8.0.0")]
